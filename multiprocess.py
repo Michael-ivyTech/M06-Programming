009e80d9f730016fd196b189c3d88437f50f7f3f
Michael-ivyTech <mbaker423@ivytech.edu>
2025-10-06 09:13:39 -0400

    Update multiprocess.py

--- multiprocess.py
+++ multiprocess.py
@@ -10,7 +10,10 @@ def whatTimeIsIt(num):
     print(f"I'm process {num}, and It's {datetime.now()}.")
 
 if __name__ == "__main__":
+    processes = []
     for n in range(3):
-        p = multiprocessing.Process(target=whatTimeIsIt, args=str(n))
+        p = multiprocessing.Process(target=whatTimeIsIt, args=(n,)) 
         p.start() 
-
+        processes.append(p)
+    p.join() 
+    print("All processes have finished.")
